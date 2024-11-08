735352690ae947dbe8d5f0cb0fb9741269643edd
D'arcy Stubbins <darcystubbins.dev.99@gmail.com>
2024-11-08 23:21:01 +0000

    frontend chnages + the post card now renders the html

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,16 +29,20 @@
         <div class="grid grid-cols-2 gap-4">
             <!-- card displaying  -->
             @foreach ($posts as $post)
-                <div class="flex grid grid-cols-3 gap-2 p-5 border rounded-lg shadow bg-rose-50">
+                <div class="flex grid grid-cols-3 gap-2 items-center p-5 border rounded-lg shadow bg-rose-50">
                     <!-- hidden div -->
                     <div id="postBody{{ $post->id }}" class="hidden">
-                        {{ $post->blog_content }}
+                        {!! $post->blog_content !!}
                     </div>
                     <div class="cols-start-1 col-span-1 font-bold">
                         {{ $post->blog_title }}
                     </div>
                     <div class="col-start-1 col-span-2">
-                        {{ $post->blog_content }}
+                        {!! $post->blog_content !!}
+                    </div>
+                    <div class="col-start-1 col-span-1 font-bold">
+                        Posted By:
+                        <!-- Posted By: {{ $post->blog_author }} -->
                     </div>
                     <!-- 'open' button -->
                     <div class="col-start-3 col-span-1 flex justify-end">
@@ -58,16 +62,16 @@
                 <div id="modalPost{{ $post->id }}" style="display: none;"
                     class="fixed top-0 left-0 bg-black bg-opacity-60 w-full h-full">
                     <div class="p-5 border rounded-lg shadow bg-rose-50 m-10">
-                        <h5 class="text-2xl font-bold text-rose-600">My Modal</h5>
+                        <div class="text-4xl font-bold text-rose-600">{{ $post->blog_title }}</div>
+                        <div class="text-lg font-semibold text-black">
+                            Posted By:
+                            <!-- Posted By: {{ $post->blog_author }} -->
+                        </div>
+                        <p>{!! $post->blog_content !!}</p>
                         <!-- 'close' button -->
                         <button data-target="modalPost{{ $post->id }}"
                             class="close-button inline-flex self-end items-center bg-rose-600 hover:bg-rose-700 text-white font-bold py-2 px-4 rounded-full max-h-8">
-                            Close More
-                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M1 5h12m0 0L9 1m4 4L9 9" />
-                            </svg>
+                            Close
                         </button>
                     </div>
                 </div>
