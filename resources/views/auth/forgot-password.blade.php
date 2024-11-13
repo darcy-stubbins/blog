6106acc203e79c80d848e13953cd68930c90c069
D'arcy Stubbins <darcystubbins.dev.99@gmail.com>
2024-11-13 13:55:00 +0000

    more frontend chnages and deletion on breeze componenets

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -25,9 +25,6 @@
             will allow you to choose a new one.
         </div>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
 
